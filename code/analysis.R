59a28d0683c2826c56587ee3721b8526971c9519
Andrew Brooks <andrewbrooksct@gmail.com>
2014-08-05 08:30:03 -0400

    finding time2arrival for each prediction

--- code/analysis.R
+++ code/analysis.R
@@ -2,28 +2,40 @@ rm(list=ls())
 source('/Users/ajb/Documents/github/nextbus/code/idBuses.R')
 
 df <- read.delim('/Users/ajb/Documents/github/nextbus/data/bus64_1Aug2014.txt', sep='|', header=T, stringsAsFactors=F)
+
+## some pre-processing
 df$time <- as.POSIXct(df$time, origin='EST')
+df$hour <- as.numeric(strftime(df$time, '%H'))
 
-## Finding Arrivals, Departures and trip
-#df <- idBuses(dfb)
+df$TripID <- as.character(df$TripID)
+df <- df[order(df$TripID, df$time),]
 
-## plotting to validate 
-plot(df$predtime, col=df$VehicleID)
-plot(df$predtime, col=df$tripID)
-plot(df$Minutes, col=df$VehicleID, pch=ifelse(df$arrival==1 | df$departure==1, 19,3))
-plot(df$Minutes, col=df$tripID, pch=ifelse(df$arrival==1 | df$departure==1, 19,3))
-plot(df$time, df$Minutes)
+## marking arrival and departure dates for Trips
+depart <- by(df$time, df$TripID, min)
+arrive <- by(df$time, df$TripID, max)
+df$departure <- 0; df$arrival <- 0; df$est <- ß0
+for(i in 1:nrow(depart)-1) {
+  tname <- names(depart)[i]
+  df$departure[df$TripID==tname & df$time==depart[i]] <- 1
+  df$arrival[df$TripID==tname & df$time==arrive[i]] <- 1
+}
 
-## clean bus data
-dfc <- cleanBusData(df, minpredcount=10)
-plot(dfc$Minutes, col=dfc$VehicleID, pch=ifelse(dfc$arrival==1 | dfc$departure==1, 19,3))
-plot(dfc$Minutes, col=dfc$tripID, pch=ifelse(dfc$arrival==1 | dfc$departure==1, 19,3))
+# removing buses that never arrive 
+TripsThatArrive <- df$TripID[df$arrival==1 & df$Minutes==0]
+df <- df[df$TripID %in% TripsThatArrive,]
 
-## assessing errors.
-errdf <- validatePred(dfc, min=10)
-mean(errdf[,1], na.rm=T)
-mean(errdf[,2], na.rm=T)
-mean(errdf[,3], na.rm=T)
+# finding actual arrival times
+df$est <- 0
+for(i in 1:nrow(depart)) {
+  tname <- names(depart)[i]
+  df$est[df$TripID==tname] <- (df$time[df$TripID==tname & df$arrival==1] - df$time[df$TripID==tname])/60
+}
+
+# analyzing results
+plot(df$est[df$Minutes==5], col=df$TripID[df$Minutes==5], pch=19)
+hist(df$est[df$Minutes==5])
+mean(df$est[df$Minutes==5])
+
 
 
 
